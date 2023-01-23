9de14e1fda64aadf3ee4c9b2be2f7da83ffb8ad0
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-01-23 16:06:59 +0100

    refactoring code

--- js/controller.js
+++ js/controller.js
@@ -55,7 +55,134 @@ const fetchRecipe = async function (id = '41470') {
     `https://forkify-api.herokuapp.com/api/get?rId=${id}`
   );
   const data = await res.json();
-  console.log(data);
+
+  const html = `
+  <figure class="recipe__container-img">
+    <button class="back-arrow">
+      <svg class="icon--white back-arrow__icon">
+        <use xlink:href="img/icons.svg#icon-arrow-left"></use>
+      </svg>
+    </button>
+    <img src= "img/${data.image_url}" alt="recipe image" class="recipe__img" />
+    <div class="background-blend"></div>
+    <figcaption class="recipe__caption">
+      <h1 class="heading--1 recipe__title">
+        <span class="recipe__title--skew">
+          ${data.title}
+        </span>
+      </h1>
+    </figcaption>
+  </figure>
+  <div class="recipe__details">
+    <div class="recipe__time">
+      <svg class="icon icon--red">
+        <use xlink:href="img/icons.svg#icon-clock"></use>
+      </svg>
+      <p class="recipe__text"><strong>75</strong> minutes</p>
+    </div>
+    <div class="recipe__update">
+      <div class="recipe__time">
+        <svg class="icon--red icon">
+          <use xlink:href="img/icons.svg#icon-users"></use>
+        </svg>
+        <p class="recipe__text">
+          <strong><span class="recipe__servings">4</span></strong>
+          servings
+        </p>
+      </div>
+      <div class="recipe__counter-container">
+        <button class="btn btn--recipe recipe__counter">
+          <svg class="recipe__icon icon--red icon">
+            <use xlink:href="img/icons.svg#icon-minus-outline"></use>
+          </svg>
+        </button>
+        <button class="btn btn--recipe recipe__counter">
+          <svg class="recipe__icon icon--red icon">
+            <use xlink:href="img/icons.svg#icon-plus-circle"></use>
+          </svg>
+        </button>
+      </div>
+      <button class="btn recipe__bookmark btn--add-bookmark">
+        <svg class="icon--white icon">
+          <use xlink:href="img/icons.svg#icon-bookmark"></use>
+        </svg>
+      </button>
+    </div>
+  </div>
+  <div class="recipe__ingredients">
+    <h2 class="heading--2 recipe__heading-title">recipe ingredients</h2>
+    <ul class="recipe__ingredients-list">
+      <li class="recipe__ingredients-item">
+        <svg class="icon icon--red">
+          <use xlink:href="img/icons.svg#icon-check"></use>
+        </svg>
+        <p class="recipe__ingredients-item__text">
+          4 oz cream cheese room temperature
+        </p>
+      </li>
+      <li class="recipe__ingredients-item">
+        <svg class="icon icon--red">
+          <use xlink:href="img/icons.svg#icon-check"></use>
+        </svg>
+        <p class="recipe__ingredients-item__text">1/4 cup mayonnaise</p>
+      </li>
+      <li class="recipe__ingredients-item">
+        <svg class="icon icon--red">
+          <use xlink:href="img/icons.svg#icon-check"></use>
+        </svg>
+        <p class="recipe__ingredients-item__text">
+          1/4 cup parmigiano reggiano grated
+        </p>
+      </li>
+      <li class="recipe__ingredients-item">
+        <svg class="icon icon--red">
+          <use xlink:href="img/icons.svg#icon-check"></use>
+        </svg>
+        <p class="recipe__ingredients-item__text">
+          1/2 cup mozzarella shredded/grated
+        </p>
+      </li>
+      <li class="recipe__ingredients-item">
+        <svg class="icon icon--red">
+          <use xlink:href="img/icons.svg#icon-check"></use>
+        </svg>
+        <p class="recipe__ingredients-item__text">
+          1/4 cup parmigiano reggiano grated
+        </p>
+      </li>
+      <li class="recipe__ingredients-item">
+        <svg class="icon icon--red">
+          <use xlink:href="img/icons.svg#icon-check"></use>
+        </svg>
+        <p class="recipe__ingredients-item__text">
+          2 tbsps black olives sliced
+        </p>
+      </li>
+    </ul>
+  </div>
+  <div class="recipe__cook">
+    <h2 class="heading--2 recipe__heading-title">how to cook it</h2>
+    <p class="recipe__cook-text">
+      This recipe was carefully designed and tested by
+      <strong> ${data.publisher}</strong>. Please check out directions at
+      their website.
+    </p>
+    <div class="recipe__cook-direction">
+      <a
+        href="${data.source_url}"
+        target="_blank"
+        class="btn btn--link btn--direction"
+      >
+        directions &nbsp;
+        <svg class="icon icon--white">
+          <use xlink:href="img/icons.svg#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </div>
+  </div>`;
+  recipeContainer.innerHTML = '';
+  const markUp = recipeContainer.insertAdjacentHTML('afterend', html);
+  return markUp;
 };
 
 fetchRecipe();
