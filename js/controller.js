ed01bb041fdeb3320c83586adc060967f07a9bd5
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-01-24 17:26:50 +0100

    Refactoring code

--- js/controller.js
+++ js/controller.js
@@ -6,7 +6,6 @@ import responsiveView from './views/responsiveView.js';
 const showRecipe = async function () {
   // 1) getting value from url that contain hash
   const id = window.location.hash.slice(1);
-  console.log(id);
 
   // 2) if the url does not contain hash return
   if (!id) return;
@@ -14,11 +13,11 @@ const showRecipe = async function () {
   // 3) passing hash value to the recipe promise (fetchRecipe)
   const recipeData = await fetchRecipe(id);
 
-  // 4) Calling recipe handler to pass the recipe promise (fetchRecipe) as an argument
-  RecipeView.recipeHandler(recipeData);
-
-  // 5) Rendering recipe view to user interface
+  // 4) Rendering recipe view to user interface
   RecipeView.render(recipeData);
+
+  // 5) Calling recipe handler to pass the recipe promise (fetchRecipe) as an argument
+  RecipeView.recipeHandler(recipeData);
 };
 
 // initialization function: get call when ever the page load
