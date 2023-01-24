d9fc6896aefbb71f82cf7c38d1d6badc1a43d7c9
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-01-24 16:49:12 +0100

    Refactoring code

--- js/model.js
+++ js/model.js
@@ -3,20 +3,20 @@
 // Building of a reject promise
 const rejectPromise = function (sec) {
   return new Promise(function (_, reject) {
-    setTimeout(reject('The request take too long time 💥💥💥'), sec);
+    reject('The request take too long time 💥💥💥');
   });
 };
 
 // Consuming a recipe promise
 export const fetchRecipe = async function (id) {
   try {
-    // const res = await Promise.race([
-    //   fetch(`https://forkify-api.herokuapp.com/api/get?rId=${id}`),
-    //   rejectPromise(10),
-    // ]);
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/get?rId=${id}`
-    );
+    const res = await Promise.race([
+      fetch(`https://forkify-api.herokuapp.com/api/get?rId=${id}`),
+      rejectPromise(),
+    ]);
+    // const res = await fetch(
+    //   `https://forkify-api.herokuapp.com/api/get?rId=${id}`
+    // );
 
     console.log(res);
     const data = await res.json();
