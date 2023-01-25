0beca0faf7f66af392f6017429bb304d2868e69e
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-01-25 07:22:20 +0100

    refactoring code

--- js/model.js
+++ js/model.js
@@ -26,10 +26,11 @@ export const fetchRecipe = async function (id) {
 
     // storing data
     const data = await res.json();
+    console.log(data);
 
     // destructing the data
-    const { recipe } = data.data;
-    console.log(recipe);
+    // const { recipe } = data.data;
+    // console.log(recipe);
 
     // renaming the data
     // state.recipe = {
