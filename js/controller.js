f2e5b51d0accd9d6caa2e229642f03cd10b7a694
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-01-25 12:42:02 +0100

    refactoring code

--- js/controller.js
+++ js/controller.js
@@ -11,13 +11,13 @@ const showRecipe = async function () {
   if (!id) return;
 
   // 3) Rendering spinner to the user interface
-  // RecipeView.renderSpinner();
+  RecipeView.renderSpinner();
 
   // 4) passing hash value to the recipe promise (fetchRecipe)
   await model.fetchRecipe(id);
 
   // 5) Rendering recipe view to user interface
-  // RecipeView.render(model.state.recipe);
+  RecipeView.render(model.state.recipe);
 };
 
 // initialization function: get call when ever the page load
