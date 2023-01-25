1868887e62f03165c7588b1b6a9fba997d077aac
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-01-25 17:35:12 +0100

    refactoring code

--- js/views/recipeItemView.js
+++ js/views/recipeItemView.js
@@ -6,13 +6,11 @@ class RecipeItemView extends View {
 
   #generateHtml() {
     return `
-    ${this._data.forEach(i => {
-      console.log(i);
-      `<a href="#${i.id}" class="recipe-item__link link">
+        <a href="#${this._data.id}" class="recipe-item__link link">
             <li class="recipe-item__details">
                 <figure class="recipe-item__container">
                 <img
-                    src="${i.imageUrl}"
+                    src="${this._data.imageUrl}"
                     alt="Recipe item image"
                     class="recipe-item__img"
                 />
@@ -21,12 +19,11 @@ class RecipeItemView extends View {
                 ></div>
                 </figure>
                 <div class="recipe-item__content">
-                <h4 class="heading--4 recipe-item__title">${i._data.title}</h4>
-                <p class="recipe-item__text">${i._data.publisher}</p>
+                <h4 class="heading--4 recipe-item__title">${this._data.title}</h4>
+                <p class="recipe-item__text">${this._data.publisher}</p>
                 </div>
             </li>
-        </a>`;
-    })}
+        </a>
     
     `;
   }
