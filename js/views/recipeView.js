376cf016209afb572487dcddad057ffefae17fc9
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-01-26 15:08:20 +0100

    importing fractional from npm

--- js/views/recipeView.js
+++ js/views/recipeView.js
@@ -1,9 +1,9 @@
 import icon from 'url:../../img/icons.svg';
 console.log(icon);
-import Fraction from 'fractional';
-console.log(Fraction);
-
 import View from './view';
+
+import { Fraction } from 'fractional';
+console.log(Fraction);
 
 class RecipeView extends View {
   _parentEl = document.querySelector('.recipe');
