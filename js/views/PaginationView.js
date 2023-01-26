8dfcce3cac635ec8d36c7cf24235201f4367407a
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-01-26 17:04:41 +0100

    displayed pagination button

--- js/views/PaginationView.js
+++ js/views/PaginationView.js
@@ -11,12 +11,12 @@ class PaginationView extends View {
     console.log(pageLength);
     if (pageLength < this._data.length) {
       return `
-        <button class="btn pagination__prev btn--change btn--pagination">
-            <svg class="icon icon--red">
-            <use xlink:href="${icon}#icon-arrow-left"></use>
-            </svg>
-            page &nbsp; <span class="btn-count">1</span>
-        </button>
+            <button class="btn pagination__next btn--change btn--pagination">
+                page &nbsp; <span class="btn-count">2</span>
+                <svg class="icon icon--red">
+                    <use xlink:href="${icon}#icon-arrow-right"></use>
+                </svg>
+            </button>
         `;
     }
   }
