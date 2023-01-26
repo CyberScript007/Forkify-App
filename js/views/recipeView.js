5b05ce28aee94584f79bd9c13f3a1c6204a2f989
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-01-26 14:07:54 +0100

    importing fractional from npm

--- js/views/recipeView.js
+++ js/views/recipeView.js
@@ -105,7 +105,9 @@ class RecipeView extends View {
           <use xlink:href="${icon}#icon-check"></use>
         </svg>
         <p class="recipe__ingredients-item__text">
-          <span class="recipe__ingredients-item__text--quantity">${i.quantity ? new Fraction(i.quantity) : " "}</span>
+          <span class="recipe__ingredients-item__text--quantity">${
+            i.quantity ? new Fraction(i.quantity).toString() : ' '
+          }</span>
           <span class="recipe__ingredients-item__text--unit">${i.unit}</span>
           <span class="recipe__ingredients-item__text--description">${
             i.description
