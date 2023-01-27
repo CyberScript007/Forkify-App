5e7ea83d2c79ec9ec5782e92d7e1e24ac5e7b0b3
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-01-27 12:28:52 +0100

    rendering pagination button to UI

--- js/controller.js
+++ js/controller.js
@@ -41,7 +41,7 @@ const showRecipeItem = async function (query) {
     await model.fetchSearchRecipe(query);
 
     // 3) Rendering recipe item to user interface
-    RecipeItemView.render(model.getSearchResultPage(2));
+    RecipeItemView.render(model.getSearchResultPage(3));
 
     // 4) Rendring pagination button to user interface
     PaginationView.render(model.state.search);
