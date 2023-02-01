09a9a74377dedcc5cad54c82e104fa133fccb48a
busarshakrullahi <busarishakrullahi007@gmail.com>
2023-02-01 17:15:07 +0100

    displaying modal window

--- js/helper.js
+++ js/helper.js
@@ -15,7 +15,7 @@ export const helperFetchApi = async function (url, upload = undefined) {
       ? fetch(url, {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': application / json,
           },
           body: JSON.stringify(upload),
         })
